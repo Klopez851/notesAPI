3d8bb936fc3caceb19fdbfbd5526605f8935c6e7
klopez851 <keidy081lopez@gmail.com>
2026-02-16 06:08:56 -0500

    worked on config file

--- src/main/java/com/example/notesAPI/config/SecurityConfig.java
+++ src/main/java/com/example/notesAPI/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,11 +13,14 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http.csrf();
-        http.authorizeHttpRequests(request -> request.anyRequest().authenticated()); // this makes it so that ALL requests need to be authenticated
-        http.httpBasic(Customizer.withDefaults()); // requires login w/ postman
+        return http.csrf(customizer -> customizer.disable())
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated()) // this makes it so that ALL requests need to be authenticated
+                .httpBasic(Customizer.withDefaults()) // requires login w/ postman
+                .sessionManagement(
+                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
 
 
-        return http.build(); //returns a securityFilterChainObject
+//        return http.build(); //returns a securityFilterChainObject
     }
 }
