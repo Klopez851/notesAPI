d8c0719b677315f2b5040214c4f416608f492039
klopez851 <keidy081lopez@gmail.com>
2026-03-04 05:04:22 -0500

    added UserDetailsService and part of AuthenticationProvider to config file

--- src/main/java/com/example/notesAPI/config/SecurityConfig.java
+++ src/main/java/com/example/notesAPI/config/SecurityConfig.java
@@ -2,10 +2,15 @@ package com.example.notesAPI.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,5 +27,17 @@ public class SecurityConfig {
 
 
 //        return http.build(); //returns a securityFilterChainObject
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+//        provider.us
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new InMemoryUserDetailsManager(); //this is an inbuilt implementation of the UserDetailsService interface, which allows us to move foward with coding w/o having to make a specific implementation just yet
     }
 }
