6d59b3ad1bd4ec5a65a72c72983319efbec4c5cd
klopez851 <keidy081lopez@gmail.com>
2026-03-30 05:20:48 -0400

    Replace runtime-generated JWT secret with fixed value to maintain token validity

--- src/main/java/com/example/notesAPI/service/JWTService.java
+++ src/main/java/com/example/notesAPI/service/JWTService.java
@@ -24,25 +24,24 @@ import java.util.function.Function;
 @Service
 public class JWTService {
 
-//    @Value("${JWT_KEY}")
+    @Value("${JWT_KEY}")
     private String secretKey;
 
     public JWTService(){
-        try{
-            KeyGenerator keyGenerator = KeyGenerator.getInstance("HmacSHA256");//the key needs to be a certain size for this algo
-            SecretKey sk = keyGenerator.generateKey();
-            secretKey= Base64.getEncoder().encodeToString(sk.getEncoded()); //key is first generated and encoded in base64
-
-        }catch (Exception e){
-            System.out.println(e);
-        }
+//        try{
+//            KeyGenerator keyGenerator = KeyGenerator.getInstance("HmacSHA256");//the key needs to be a certain size for this algo
+//            SecretKey sk = keyGenerator.generateKey();
+//            secretKey= Base64.getEncoder().encodeToString(sk.getEncoded()); //key is first generated and encoded in base64
+//        }catch (Exception e){
+//            System.out.println(e);
+//        }
 
     }
 
     public String generateToken(String email){
         //String = key, object = value bc it will be extracted from different objects depending on the user
         Map<String,Object> claims = new HashMap<>();//used to add custom claims to the jwt payload
-
+        System.out.println(secretKey);
         return Jwts.builder()
                 .claims()
                 .add(claims)
