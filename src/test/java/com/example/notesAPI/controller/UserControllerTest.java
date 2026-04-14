46d12bcce5695cd29d3b4d84148f495482e32bdc
klopez851 <keidy081lopez@gmail.com>
2026-04-14 05:00:03 -0400

    created more userController tests

--- src/test/java/com/example/notesAPI/controller/UserControllerTest.java
+++ src/test/java/com/example/notesAPI/controller/UserControllerTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import io.restassured.http.Header;
+import org.springframework.http.HttpStatus;
+
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
@@ -33,6 +35,101 @@ class UserControllerTest {
             .then() //what needs to be asserted/validated/tested for correctness
                 .extract().body().asString();
     }
+
+    /////////////////////////
+    /// CREATE USER TESTS ///
+    /////////////////////////
+
+    @Test
+    void createUser_UserDoesntExists_UserIsCreated(){
+        given()
+                .when()
+                .contentType("application/json")
+                .body("""
+                        {
+                            "username":"klopez2",
+                            "email":"sampleemail2@gmail.com",
+                            "userPassword":"qwertyisfun2"
+                        }
+                        """)
+                .post("/createUser")
+                .then()
+                .assertThat()
+                .statusCode(200);
+
+    }
+
+    @Test
+    void createUser_UserDoesExists_ConflictResponse(){
+        given()
+                .when()
+                .contentType("application/json")
+                .body("""
+                        {
+                            "username":"klopez",
+                            "email":"sampleemail@gmail.com",
+                            "userPassword":"qwertyisfun"
+                        }
+                        """)
+                .post("/createUser")
+                .then()
+                .assertThat()
+                .statusCode(409);//conflict
+    }
+
+    @Test
+    void createUser_EmptyReqBody_BadReqResponse(){
+        given()
+                .when()
+                .contentType("application/json")
+                .body("")
+                .post("/createUser")
+                .then()
+                .assertThat()
+                .statusCode(400);//bad request
+    }
+
+    @Test
+    void createUser_UsernameTooLong_BadReqResponse(){
+        given()
+                .when()
+                .contentType("application/json")
+                .body("""
+                        {
+                            "username":"klopezklopezklopezklopezkl(51 chars including this)",
+                            "email":"sampleemail@gmail.com",
+                            "userPassword":"qwertyisfun"
+                        }
+                        """)
+                .post("/createUser")
+                .then()
+                .assertThat()
+                .statusCode(400);//bad request
+    }
+
+    @Test
+    void createUser_EmailTooLong_BadReqResponse(){
+        given()
+                .when()
+                .contentType("application/json")
+                .body("""
+                        {
+                            "username":"klopez",
+                            "email":"sampleemail@gmail.comsampleemail@gmail.comsampleemail@gmail.comsampleemail@gmail.com
+                            sampleemail@gmail.comsampleemail@gmail.comsampleemail@gmail.comsampleemail@gmai
+                            (this is 255 chars including this)",
+                            "userPassword":"qwertyisfun"
+                        }
+                        """)
+                .post("/createUser")
+                .then()
+                .assertThat()
+                .statusCode(400);//bad request
+    }
+
+
+
+
 
     @Test
     void getUser_ProperBody_ReturnsSuccessMsg(){
